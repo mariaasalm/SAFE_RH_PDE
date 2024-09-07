f2b848313a4cafad701b0e283451341503724f8b
MARIA ASLAM <74291938+mariaasalm@users.noreply.github.com>
2024-09-07 13:12:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,37 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import style_css as css
 import plotly as px
-# def add_custom_css():
-#     st.markdown("""
-#         <style>
-#         /* Center align the header */
-#         h1 {
-#             color: #4CAF50;
-#             text-align: center;
-#         }
 
-#         /* Customize input fields */
-#         input {
-#             border-radius: 10px;
-#             padding: 10px;
-#             margin-bottom: 10px;
-#             border: 1px solid #ccc;
-#         }
-
-#         /* Customize buttons */
-#         button {
-#             background-color: #4CAF50;
-#             color: white;
-#             border-radius: 10px;
-#             padding: 10px 20px;
-#         }
-
-#         /* Change background color */
-#         .stApp {
-#             background-color: #F0F8FF;
-#         }
-#         </style>
-#     """, unsafe_allow_html=True)
 
 # Adding custom CSS
 css.add_custom_css()
