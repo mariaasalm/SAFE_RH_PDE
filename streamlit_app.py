2081c19a3b56d5362be769c88eb4e2fd18989a19
MARIA ASLAM <74291938+mariaasalm@users.noreply.github.com>
2024-09-07 10:11:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+
+
 import streamlit as st
 import mysql.connector
 from mysql.connector import Error
@@ -6,26 +8,18 @@ from mysql.connector import Error
 def connect_to_database():
     try:
         conn = mysql.connector.connect(
-            host='u458377734_icu_bvh_paeds.hostinger.com',
-            user='u458377734_maria',
-            password= 'Mscs2017',
-            database= 'u458377734_icu_bvh_paeds'
+        host='u458377734_icu_bvh_paeds.hostinger.com',
+        user='u458377734_maria',
+        password= 'Mscs2017',
+        database= 'u458377734_icu_bvh_paeds'
         )
         if conn.is_connected():
-            return True
+            st.success("Connected to the database!")
     except Error as e:
-        return False
+        st.error(f"Failed to connect to the database. Error: {str(e)}")
+    finally:
+        if conn.is_connected():
+            conn.close()
 
-# Main Streamlit App
-def main():
-    st.title("Database Connection Test")
-
-    # Check database connection
-    if connect_to_database():
-        st.success("Database connected successfully!")
-    else:
-        st.error("Failed to connect to the database.")
-
-if __name__ == '__main__':
-    main()
-
+st.title("Database Connection Test")
+connect_to_database()
