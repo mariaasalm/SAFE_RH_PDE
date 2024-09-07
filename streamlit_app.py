ba4bbb9a8c6e0a768ea8e48e7226a74eebca25c9
MARIA ASLAM <74291938+mariaasalm@users.noreply.github.com>
2024-09-07 13:07:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,42 +4,42 @@ import mysql.connector
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import style_css as css
 import plotly as px
-def add_custom_css():
-    st.markdown("""
-        <style>
-        /* Center align the header */
-        h1 {
-            color: #4CAF50;
-            text-align: center;
-        }
+# def add_custom_css():
+#     st.markdown("""
+#         <style>
+#         /* Center align the header */
+#         h1 {
+#             color: #4CAF50;
+#             text-align: center;
+#         }
 
-        /* Customize input fields */
-        input {
-            border-radius: 10px;
-            padding: 10px;
-            margin-bottom: 10px;
-            border: 1px solid #ccc;
-        }
+#         /* Customize input fields */
+#         input {
+#             border-radius: 10px;
+#             padding: 10px;
+#             margin-bottom: 10px;
+#             border: 1px solid #ccc;
+#         }
 
-        /* Customize buttons */
-        button {
-            background-color: #4CAF50;
-            color: white;
-            border-radius: 10px;
-            padding: 10px 20px;
-        }
+#         /* Customize buttons */
+#         button {
+#             background-color: #4CAF50;
+#             color: white;
+#             border-radius: 10px;
+#             padding: 10px 20px;
+#         }
 
-        /* Change background color */
-        .stApp {
-            background-color: #F0F8FF;
-        }
-        </style>
-    """, unsafe_allow_html=True)
+#         /* Change background color */
+#         .stApp {
+#             background-color: #F0F8FF;
+#         }
+#         </style>
+#     """, unsafe_allow_html=True)
 
 # Adding custom CSS
-add_custom_css()
+css.add_custom_css()
 # Function to connect to the database
 def get_db_connection():
     return mysql.connector.connect(
