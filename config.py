c4b08e708226184e749b7b3f6a88ace8337993b5
MARIA ASLAM <74291938+mariaasalm@users.noreply.github.com>
2024-09-22 17:37:59 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-
+# Add patient form
+import streamlit as st
+import mysql.connector
+def get_db_connection():
+    return mysql.connector.connect(
+        # Replace these with your actual credentials and settings
+    host="id-dci-web1158.main-hosting.eu",
+    user="u458377734_maria",
+    password="Mscs2017",
+    database="u458377734_icu_bvh_paeds"
+    )
